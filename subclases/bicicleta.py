73ea1973701e2a7eb37374749c20c779f5381880
MarcoUnie <mmagecoyt@gmail.com>
2025-03-19 19:32:44 +0100

    Elementos arreglados y puestos en subclases

--- subclases/bicicleta.py
+++ subclases/bicicleta.py
@@ -1,9 +1,9 @@
-from superclases import vehiculo
-class Bicicleta(vehiculo):
+from superclasess.Vehiculos import Vehiculo
+class Bicicleta(Vehiculo):
     def __init__(self, color, ruedas, tipo):
         super().__init__(color, ruedas)
         self.tipo = tipo
     def __str__(self):
-        return f"{super().__str()}, {self.tipo}"
+        return f"{super().__str__()}, {self.tipo}"
     def __name__(self):
         return "Bicicleta"
--- subclases/coche.py
+++ subclases/coche.py
@@ -1,10 +1,10 @@
-from superclases import vehiculo
-class Coche(vehiculo):
+from superclasess.Vehiculos import Vehiculo
+class Coche(Vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada):
         super().__init__(color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
-        return f"{super().__str()}, {self.velocidad} km/h, {self.cilindrada} cc"
+        return f"{super().__str__()}, {self.velocidad} km/h, {self.cilindrada} cc"
     def __name__(self):
         return "Coche"
