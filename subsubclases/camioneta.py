bd03ed296052e6b5aca6411b128a1e51de2283c9
MarcoUnie <mmagecoyt@gmail.com>
2025-03-19 19:40:53 +0100

    Ultimos arreglos y funcion catalogar

--- subsubclases/camioneta.py
+++ subsubclases/camioneta.py
@@ -1,9 +1,9 @@
-from subclases import coche
-class Camioneta(coche):
+from subclases.coche import Coche
+class Camioneta(Coche):
     def __init__(self, color, ruedas, velocidad, cilindrada, carga):
         super().__init__(color, ruedas, velocidad, cilindrada)
         self.carga = carga
     def __str__(self):
-        return f"{super().__str()}, {self.carga} kg"
+        return f"{super().__str__()}, {self.carga} kg"
     def __name__(self):
         return "Camioneta"
--- subsubclases/motocicleta.py
+++ subsubclases/motocicleta.py
@@ -1,10 +1,10 @@
-from subclases import bicicleta
-class Motocicleta(bicicleta):
+from subclases.bicicleta import Bicicleta
+class Motocicleta(Bicicleta):
     def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
         super().__init__(color, ruedas, tipo)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
-        return f"{super().__str()}, {self.velocidad} km/h, {self.cilindrada} cc"
+        return f"{super().__str__()}, {self.velocidad} km/h, {self.cilindrada} cc"
     def __name__(self):
         return "Motocicleta"
